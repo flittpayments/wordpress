fe5a4097c86626617dfb7500a7af0fbd63471aaf
Dmitriy <dmmatrix222@gmail.com>
2025-12-19 14:49:00 +0200

    Remove translation function from order description

--- woocommerce/flitt-woocommerce-payment-gateway/includes/abstract-wc-flitt-payment-gateway.php
+++ woocommerce/flitt-woocommerce-payment-gateway/includes/abstract-wc-flitt-payment-gateway.php
@@ -327,7 +327,7 @@ class WC_Flitt_Payment_Gateway extends WC_Payment_Gateway
     {
         $params = [
             'order_id' => $this->createFlittOrderID($order),
-            'order_desc' => __('Order: ', 'flitt-woocommerce-payment-gateway') . $order->get_id(),
+            'order_desc' => 'Order: ' . $order->get_id(),
             'amount' => (int)round($order->get_total() * 100),
             'currency' => get_woocommerce_currency(),
             'lang' => $this->getLanguage(),
